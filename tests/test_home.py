d2f14f335311059ef132d904624b1746bd6e081d
Ewerton Petillo <ewertonpetillo@ufam.edu.br>
2024-08-05 14:18:48 -0400

    testes

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,7 +1,7 @@
 import time
 
 def test_home(browser):
-    url = "http://0.0.0.0:8080/"
+    url = "http://0.0.0.0:8081/"
     browser.get(url)
     time.sleep(10)
     assert browser.title == "Dash"
